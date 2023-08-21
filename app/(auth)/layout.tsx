41a10e399f9fa38204a239e0629981271e777682
rajibjonchhen <rajib.jonchhen@gmail.com>
2023-08-21 14:20:14 +0100

    sign-up, sign-in, onboarding routes added with google/provides auth

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import "../globals.css"
+import {ClerkProvider} from '@clerk/nextjs'
+
+export const metadata: Metadata = {
+  title: 'Threads',
+  description: 'A thread-clone app with next.js',
+}
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+      <ClerkProvider>
+        <html lang="en">
+            <body className={`${inter.className} bg-dark-1`}>
+                {children}
+            </body>
+        </html>
+      </ClerkProvider>
+  )
+}
---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,8 @@
+async function Page (){
+    return (
+        <main>
+            <h1>onboarding</h1>
+        </main>
+    )
+}
+export default Page
