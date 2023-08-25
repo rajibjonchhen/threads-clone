23510b8f68781c7123dc41a515cc814d67195369
rajibjonchhen <rajib.jonchhen@gmail.com>
2023-08-25 16:35:15 +0100

    top bar dark added from clerk/theme to get the dark theme

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -2,6 +2,7 @@ import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
 import SignOutBtn from "./SignOutBtn"
+import { dark } from "@clerk/themes"
 
 function Topbar() {
   return (
@@ -20,6 +21,7 @@ function Topbar() {
         </div>
         <OrganizationSwitcher 
           appearance={{
+            baseTheme : dark,
             elements:{
               organizationSwitcherTrigger: "py-2 px-4"
             }
