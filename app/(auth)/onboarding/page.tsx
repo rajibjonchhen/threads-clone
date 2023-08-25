925471921b42b76ce7fb662fcab4ea3147b5883e
rajibjonchhen <rajib.jonchhen@gmail.com>
2023-08-25 16:36:32 +0100

    onboarding page initialised

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,7 +1,25 @@
+import AccountProfile from "@/components/forms/AccountProfile"
+import {currentUser} from "@clerk/nextjs"
+import { userInfo } from "os"
+
 async function Page (){
+    const user = await currentUser()
+    const userData = {
+        id : user?.id,
+        objectId : userInfo?.id,
+        username : userInfo?.username || user?.username,
+        username : userInfo?.name || user?.firstname || "",
+        bio : userInfo?.bio || "",
+        image : userInfo?.image || user?.imageUrl,
+        
+    }
     return (
-        <main>
-            <h1>onboarding</h1>
+        <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
+            <h1 className="head-text">onboarding</h1>
+            <p className="mt-3 text-base-regular text-light-2">Complete your profile now to use Threads</p>
+            <section className="mt-9 bg-dark-2 p-10">
+                <AccountProfile user = {userData} btnTitle = "Continue"/>
+            </section>
         </main>
     )
 }
