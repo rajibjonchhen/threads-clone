ab3490325b62347856a298abecb1b4de7f65aaa7
rajibjonchhen <rajib.jonchhen@gmail.com>
2023-09-06 15:26:46 +0100

    tabs components addecd

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,6 +1,10 @@
 import PostThread from "@/components/forms/PostThread"
+import ProfileHeader from "@/components/shared/ProfileHeader"
+import { Tabs, TabsList, TabsContent, TabsTrigger } from "@/components/ui/tabs"
+import { profileTabs } from "@/contants"
 import { fetchUser } from "@/lib/actions/user.actions"
 import { currentUser } from "@clerk/nextjs"
+import Image from "next/image"
 import { redirect } from "next/navigation"
 async function Page({params}:{params : {id : string}}) {
     const user = await currentUser()
@@ -10,7 +14,26 @@ async function Page({params}:{params : {id : string}}) {
     if(!userInfo?.onboarded) redirect('/onboarding');
     return ( 
         <section>
-            Profile
+            <ProfileHeader
+            accountId = {userInfo.id}
+            authUserId = {user.id}
+            name = {userInfo.name}
+            username = {userInfo.username}
+            imgUrl = {userInfo.image}
+            bio = {userInfo.bio}
+            />
+            <div className="mt-9">
+                <Tabs defaultValue = "threads" className = "w-full">
+                    <TabsList className = "tab">
+                    {profileTabs.map((tab) => (
+                        <TabsTrigger key = {tab.label} value = {tab.value}  className = "tab">
+                            <Image src = {tab.icon} width = {24} height = {24} alt = {`${tab.label} icon`}/>
+                            <p className="max-sm:hidden">{tab.label}</p>
+                        </TabsTrigger>
+                    ))}
+                    </TabsList>
+                </Tabs>
+            </div>
         </section>
      );
 }
